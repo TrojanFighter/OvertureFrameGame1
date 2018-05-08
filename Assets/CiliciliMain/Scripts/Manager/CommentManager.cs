f785d077b64897418a54730ae87fa79d395fd24a
Sparky <timdao@hotmail.com>
2018-05-08 14:26:47 -0400

    Editor if

--- Assets/CiliciliMain/Scripts/Manager/CommentManager.cs
+++ Assets/CiliciliMain/Scripts/Manager/CommentManager.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Overture.FrameGame;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
